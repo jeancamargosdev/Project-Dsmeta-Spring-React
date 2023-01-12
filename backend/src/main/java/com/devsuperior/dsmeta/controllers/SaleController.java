28f3b480f815b4fe79d5c9727d5e8d22c590e64b
jmcamargos <jeanmatheus@hotmail.com>
2023-01-12 10:11:45 -0300

    twillio sms

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,17 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
-
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
-import org.hibernate.annotations.GeneratorType;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -20,7 +14,8 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
-
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -28,6 +23,11 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) { //na web os dados trafegam como texto, por isso minDate e maxDate são string e não LocalDate
         return service.findSales(minDate, maxDate, pageable);
-
     }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+    }
+
 }
