a8d20394f104931d4def905d556928f516fc8edc
jmcamargos <jeanmatheus@hotmail.com>
2023-01-13 10:45:24 -0300

    Data updade

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,13 +18,20 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-       axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0,10);
+        const dmax = maxDate.toISOString().slice(0,10);
+
+
+
+
+       axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
        // axios.get("http://localhost:8080/sales")
             .then(response => {
                // console.log(response.data)
                setSales(response.data.content);
             })
-    }, []);
+    }, [minDate, maxDate]);
 
 
     return (
