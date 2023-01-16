1ade36c21748daf0e5b2e0d02dba585686bb90fe
jmcamargos <jeanmatheus@hotmail.com>
2023-01-16 14:17:04 -0300

    netlify final

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,5 +8,5 @@ window.React = React
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
